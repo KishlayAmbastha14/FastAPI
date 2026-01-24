6f8b916c3e8e29e18e55c638d50caf2d14dc6ff9
KishlayAmbastha14 <kishlaykumar087@gmail.com>
2026-01-24 17:58:24 +0530

    mistakes was there on tables

--- database/main.py
+++ database/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends,HTTPException
 from . import crud,models,schemas
 from .database import Base,engine,SessionLocal
 from sqlalchemy.orm import Session
@@ -22,12 +22,35 @@ async def create_user(user:schemas.UserCreate,
 @app.get("/user_get",response_model=list[schemas.UserResponse])
 async def get_user(db: Session = Depends(get_db)):
   return crud.get_users(db)
+
+# @app.delete("/deleted_user/{user_id}",response_description='this user id is delted')
+# async def delted_user(user_id:int,db:Session=Depends(get_db)):
+#   db_delete_user = crud.delete_user(db,user_id)
+
+#   print(db_delete_user)
+
+#   if not db_delete_user:
+#     raise HTTPException(status_code=404,detail="No user found sorry")
+  
+@app.delete("/delete_user/{user_id}")
+async def delete_user(user_id: int,
+                      db: Session = Depends(get_db)):
+    deleted_user = crud.delete_user(db, user_id)
+
+    if not deleted_user:
+        return {"message": "User not found"}
+
+    return {
+        "message": "User deleted successfully",
+        "deleted_user_id": deleted_user.id
+    }
+
   
 @app.post("/login_user",response_description="you are allowed to enter")
 async def login_user(user:schemas.Login_user,
                      db : Session = Depends(get_db)):
   
-  db_user = crud.login_user(db,user.email,user.password)
+  db_user = crud.login_user(db,user)
   print(db_user)
   if not db_user:
     return {"message":"invalid email or password"}
--- database/models.py
+++ database/models.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column,Integer, String
 
 class User(Base):
   __tablename__ = "users"
-  ids = Column(Integer,primary_key=True,index=True)
+  id = Column(Integer,primary_key=True,index=True)
   email = Column(String,unique=True,index=True,nullable=False)
   password = Column(String,nullable=False)
 
