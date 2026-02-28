66d21887ea5dbc1a857a781560dc1b70789a18a2
KishlayAmbastha14 <kishlaykumar087@gmail.com>
2026-02-28 23:55:52 +0530

    leran path of jwt

--- backends/security/JWT_AUTH.py
+++ backends/security/JWT_AUTH.py
@@ -17,3 +17,43 @@ from pwdlib import PasswordHash
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$argon2id$v=19$m=65536,t=3,p=4$wagCPXjifgvUFBzq4hqe3w$CYaIb8sB+wtD+Vu/P4uod1+Qof8h+1g7bbDlBID48Rc",
+        "disabled": False,
+    }
+}
+
+
+class Token(BaseModel):
+  access_token : str
+  token_type  : str
+
+
+class TokenData(BaseModel):
+  username : str | None = None
+
+class User(BaseModel):
+  username : str
+  email : str | None = None
+  full_name : str | None = None
+  disabled : str | None = None
+
+class UserInDB(User):
+  hashed_password : str 
+
+
+password_hash = PasswordHash.recommended()
+
+DUMMY_HASH = password_hash.hash("dummypassword")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+
